afce01bc3c82b0d8eb785c609aef5fb9e69fff5d
Vojko Drev <vojko.drev@adacta-fintech.com>
2020-02-20 08:12:53 +0100

    Fixed process shutdown on missing data. Added missing reject.

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ const { Gitlab } = require("gitlab");
 const elegantSpinner = require('elegant-spinner');
 const frame = elegantSpinner();
 const logUpdate = require('log-update');
+// const JiraClient = require("jira-connector");
 const JiraApi = require('jira-client');
 const util = require('util')
-var Prompt = require('prompt-password');
+const Prompt = require('prompt-password');
+const colors = require('colors');
 
 function spin() {
-  logUpdate(frame());
+  // logUpdate(frame());
 }
 
 (async () => {
@@ -34,11 +36,25 @@ function spin() {
     strictSSL: false
   });
 
+  // const jira = new JiraClient({
+  //   host: "jira.adacta-group.com",
+  //   protocol: 'https',
+  //   strictSSL: false,
+  //   basic_auth: {
+  //     username: "vojkod",
+  //     password: jiraPassword
+  //   }
+  // });
+
   spin();
+
+  console.log(1);
   
   let projects = await api.Projects.all({
     search: "Sava"
   });
+  
+  console.log(2);
 
   let savaImplProject = projects.filter(p => p.name_with_namespace == "Sava / Implementation")[0];
   
@@ -46,8 +62,10 @@ function spin() {
 
   let pipelines = await api.Pipelines.all(savaImplProject.id, {
     username: "VojkoD",
-    maxPages: 2
+    maxPages: 8
   })
+
+  console.log(3);
 
   spin();
 
@@ -65,7 +83,11 @@ function spin() {
     }));
   })
 
+  console.log(4);
+
   let commits = await Promise.all(commitPromises);
+
+  console.log(5);
 
   let commitDetailPromises = [];
   
@@ -76,13 +98,23 @@ function spin() {
   
         if (jiraId) {
           try {
-            const jiraIssue = await jira.findIssue(jiraId[0]);                
+            const jiraIssue = await jira.findIssue(jiraId[0]);
+            // const jiraIssue = await jira.issue.getIssue({issueId: jiraId[0]});
+            // console.log("jiraIssue", jiraIssue)
+            // process.exit(1)
             spin();
             resolve(jiraIssue);
+
+            // if (!jiraIssue) {
+            //   reject("No jira issue found for " + jiraId[0]);
+            // } else {
+            // }
           } catch (err) {
             spin();
             reject(err);
           }
+        } else {
+          reject("No jira id");  
         }
       } else {
         reject("No jira password");
@@ -93,48 +125,70 @@ function spin() {
   commits.forEach(c => {
     commitDetailPromises.push(new Promise(async (resolve, reject) => {
       try {
+        console.log(5.1);
         let mergeReqId = /refs\/merge-requests\/(\d+)\/head/g.exec(c.last_pipeline.ref)
 
-        let result = {
+        const result = {
           commit: c,
           refName: c.last_pipeline.ref
         };
 
+        console.log(5.2);
+
         if (mergeReqId) {
-          mergeRequest = await api.MergeRequests.show(savaImplProject.id, mergeReqId[1]);
+          console.log(5.21);
+          const mergeRequest = await api.MergeRequests.show(savaImplProject.id, mergeReqId[1]);
+          console.log(5.22);
           spin();
           result.mergeRequest = mergeRequest;
           result.refName = result.mergeRequest.title; 
+          console.log(5.23);
         } 
 
         try {
+          console.log(5.3,result.refName);
           result.jiraIssue = await getJiraIssue(result.refName);
-        } catch (err) { }
-
+          console.log(5.4);
+        } catch (err) { 
+          console.log(5.411, err) 
+        }
 
         resolve(result)
+        console.log(5.5);
+
       } catch (err) {
+        console.log(5.6);
+
         spin();
         reject(err);
       }
     }));
   })
   
-  let commitDetails = await Promise.all(commitDetailPromises);
+  let commitDetails = await Promise.all(commitDetailPromises); //.catch(e => console.log(e));
+
+  console.log(7);
 
   commitDetails.forEach(cd => {
   
-    if (parseInt(cd.commit.created_at.substring(8, 10)) % 2 == 0) {
-      process.stdout.write("\x1b[33m");
-    } else {
-      process.stdout.write("\x1b[0m");
-    }
+    // if (parseInt(cd.commit.created_at.substring(8, 10)) % 2 == 0) {
+    //   process.stdout.write("\x1b[33m");
+    // } else {
+    //   process.stdout.write("\x1b[0m");
+    // }
 
-    console.log(cd.commit.created_at, "[", cd.refName, "]", 
-      cd.jiraIssue ? `[ ${cd.jiraIssue.fields.summary} ]` : "",
-      cd.commit.message)
+    let asdasd = cd.jiraIssue ? `[${cd.jiraIssue.fields.summary}] ` : "";
+    let message = `${cd.commit.created_at} [${cd.refName}] ${asdasd}${cd.commit.message}`;
+
+    if (parseInt(cd.commit.created_at.substring(8, 10)) % 2 == 0) {
+      console.log(message.cyan)
+    } else {
+      console.log(message)
+    }
   })
 
-  process.stdout.write("\x1b[0m");
+  console.log(8);
+
+  // process.stdout.write("\x1b[0m");
   
-})();
+})().catch(console.error)
